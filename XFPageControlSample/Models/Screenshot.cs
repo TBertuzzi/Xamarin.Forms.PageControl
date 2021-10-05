a3433e8d985d2c72b520dc13949c91a0ea99973d
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2021-10-05 17:52:39 -0300

    :hammer: update samples

---
+++ XFPageControlSample/Models/Screenshot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFPageControlSample
+{
+    public class Screenshot
+    {
+        public string Image { get; set; }
+    }
+}
